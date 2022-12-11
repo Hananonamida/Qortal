217e66fc89bb60efe88f8a58aeedc1a04dddb0b1
zhaoyongke <zhaoyongke@quseit.com>
2022-12-11 14:55:38 +0800

    Fixed run at

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo service atd start
+echo "at now+1min curl -X POST \"http://127.0.0.1:12391/admin/mintingaccounts\" -H \"accept: text/plain\" -H  \"X-API-KEY: 8ciXrsQETuPSk2qan1676N\" -H  \"Content-Type: text/plain\" -d \"$KEY1\"" | at now + 1 minute
+echo "at now+1min curl -X POST \"http://127.0.0.1:12391/admin/mintingaccounts\" -H \"accept: text/plain\" -H  \"X-API-KEY: 8ciXrsQETuPSk2qan1676N\" -H  \"Content-Type: text/plain\" -d \"$KEY2\"" | at now + 1 minute
 cd /qortal && java -Xmx2G -jar qortal.jar
-at now+1min curl -X POST "http://127.0.0.1:12391/admin/mintingaccounts" -H  "accept: text/plain" -H  "X-API-KEY: 8ciXrsQETuPSk2qan1676N" -H  "Content-Type: text/plain" -d "$KEY1"
-at now+1min curl -X POST "http://127.0.0.1:12391/admin/mintingaccounts" -H  "accept: text/plain" -H  "X-API-KEY: 8ciXrsQETuPSk2qan1676N" -H  "Content-Type: text/plain" -d "$KEY2"
